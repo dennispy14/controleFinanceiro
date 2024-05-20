85f1985574ef024030368f7a35acc63e901768c7
Dennispy <dennis.py@gentecontrole.com>
2024-05-20 17:48:10 -0300

    Concluido testes UsuarioService

--- src/main/java/com/dennispy/controleFinanceiro/Service/UsuarioService.java
+++ src/main/java/com/dennispy/controleFinanceiro/Service/UsuarioService.java
@@ -5,25 +5,32 @@ import com.dennispy.controleFinanceiro.exception.ErroAutenticacao;
 import com.dennispy.controleFinanceiro.exception.RegraNegocioException;
 import com.dennispy.controleFinanceiro.model.entity.Usuario;
 import com.dennispy.controleFinanceiro.model.repository.UsuarioRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@RequiredArgsConstructor
+
 @Service
 public class UsuarioService {
 
-    @Autowired
-    private UsuarioRepository usuarioRepository;
+    private final UsuarioRepository usuarioRepository;
 
-    private final Messages messages;
+    /*@Autowired
+    private final Messages messages;*/
+
+    public UsuarioService(UsuarioRepository repository) {
+        super();
+        this.usuarioRepository = repository;
+    }
 
     public Usuario autenticar(String email, String senha){
         Optional<Usuario> usuario = usuarioRepository.findByEmail(email);
-        if(!usuario.isPresent()){
-            throw new ErroAutenticacao("Usuário não encontrado para o email informado!")
+        if(usuario.isEmpty()){
+            throw new ErroAutenticacao("Usuário não encontrado para o email informado!");
         } else if(!usuario.get().getSenha().equals(senha)){
             throw new ErroAutenticacao("Senha inválida.");
         }
@@ -34,10 +41,18 @@ public class UsuarioService {
         validarEmail(usuario.getEmail());
         return usuarioRepository.save(usuario);
     }
-    public void  validarEmail(String email) {
+
+    /*public void  validarEmail(String email) {
         boolean existe = usuarioRepository.existsByEmail(email);
         if (!existe){
-            throw new RegraNegocioException(messages.get("email.nao-encontrado"));
+            throw new RegraNegocioException("Email não encontrado!");
+        }
+    }*/
+
+    public void validarEmail(String email) {
+        boolean existe = usuarioRepository.existsByEmail(email);
+        if(existe) {
+            throw new RegraNegocioException("Já existe um usuário cadastrado com este email.");
         }
     }
 
--- src/test/java/com/dennispy/controleFinanceiro/RepositoryTest/UsuarioRepositoryTest.java
+++ src/test/java/com/dennispy/controleFinanceiro/RepositoryTest/UsuarioRepositoryTest.java
@@ -5,7 +5,6 @@ import com.dennispy.controleFinanceiro.model.repository.UsuarioRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -15,7 +14,9 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import java.util.Optional;
+
+@ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
 @DataJdbcTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -55,6 +56,50 @@ public class UsuarioRepositoryTest {
     @Test
     public void devePersistirUmUsuarioBaseDeDados(){
         //cénario
-        Usuario usuario = Usuario.builder().nome("usuario").email("usuario@email.com").senha("senha").build();
+        Usuario usuario = Usuario.builder()
+                .nome("usuario")
+                .email("usuario@email.com")
+                .senha("senha")
+                .build();
+
+        //ação
+        Usuario usuarioSalvo = repository.save(usuario);
+
+        Assertions.assertThat(usuarioSalvo.getId()).isNotNull();
     }
+
+    @Test
+    public void deveBuscarUmUsuarioPorEmail(){
+        //cenario
+        Usuario usuario = criarUsuario();
+        entityManager.persist(usuario);
+
+        //verificacao
+        Optional<Usuario> result = repository.findByEmail("usuario@email.com");
+
+        Assertions.assertThat(result.isPresent()).isTrue();
+
+    }
+
+    @Test
+    public void deveRetonarVazioBuscarUsuarioPorEmailQuandoNaoExisteNaBase(){
+        //cenario
+
+        //verificacao
+        Optional<Usuario> result = repository.findByEmail("usuario@email.com");
+
+        Assertions.assertThat(result.isPresent()).isFalse();
+    }
+
+    public static Usuario criarUsuario(){
+        return Usuario
+                .builder()
+                .nome("usuario")
+                .email("usuario@email.com")
+                .senha("senha")
+                .build();
+    }
+
+
+
 }
